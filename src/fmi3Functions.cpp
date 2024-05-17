66b41dfccaba593cfcfc68c41e75bcca084f7448
luisheres <luis.heres@protonmail.com>
2024-05-17 12:22:16 +0000

    Liasion library compiled

--- src/fmi3Functions.cpp
+++ src/fmi3Functions.cpp
@@ -1,4 +1,6 @@
 #include <stdexcept>
+#include "zenoh.hxx"
+#include "zenohc.hxx"
 #include "fmi3.pb.h"
 #include "fmi3Functions.h"
 
@@ -8,11 +10,67 @@
     } catch (const std::exception &e) {                            \
         std::cerr << ERROR_MSG << e.what() << std::endl;           \
         return ERROR_RETURN;                                       \
+    } \
+
+std::string responder_id = "demo";
+
+// ZENOH 
+
+std::unique_ptr<zenoh::Session> z_client;
+
+void startZenoh(){
+    zenoh::Config config;
+    auto result = open(std::move(config));
+    if (std::holds_alternative<zenoh::Session>(result)) {
+        z_client = std::make_unique<zenoh::Session>(std::move(std::get<zenoh::Session>(result)));
+        std::cout << "Zenoh Session started successfully." << std::endl;
+    } else if (auto error = std::get_if<zenoh::ErrorMessage>(&result)) {
+         throw std::runtime_error(std::string(error->as_string_view()));
+    } else {
+        std::cerr << "Error: Unknown." << std::endl;
     }
+}
 
 
-// Zenoh client
-std::unique_ptr<Session> session;
+// This macro makes a remote procedure call in the form of a query
+// to Zenoh queryable. The argument fmi3Function is the name of the
+// function to be called in the remote resource. An variable named 
+// 'input' is the input to said function and should be an instance
+// of a Protobuf message type.
+// This macro requires that:
+//  1.  A variable 'responder_id' of type std::string is declared
+//      and defined following the key expression specification of 
+//      Zenoh (e.g. std::string responder_id = "foo/bar").
+//  1.  A variable 'input' of a certain Protobuf message type
+//      is declared and defined (e.g. proto::Instance input; 
+//      input.set_key(1);)
+//  2. A variable 'output' of a certain Protobuf messate type 
+//     is declared (e.g. proto::Instance output;).
+#define ZENOH_FMI3_QUERY(fmi3Function) \
+    size_t input_size = input.ByteSizeLong(); \
+    std::vector<uint8_t> buffer(input_size); \
+    input.SerializeToArray(buffer.data(), input_size); \
+    std::string expr = "rpc/" + responder_id + "/" + fmi3Function; \
+    auto [send, recv] = zenohc::reply_fifo_new(1);   \
+    const char* params = ""; \
+    zenoh::GetOptions options; \
+    zenoh::Value value(buffer); \
+    options.set_value(value); \
+    z_client->get(          \
+        expr,    \
+        params,             \
+        std::move(send),            \
+        options);                        \
+    zenoh::Reply reply(nullptr);  \
+    recv(reply);  \
+    if (reply.is_ok()) { \
+        zenohc::Sample sample = zenohc::expect<zenoh::Sample>(reply.get()); \
+        output.ParseFromArray(sample.payload.start, sample.payload.len); \
+    } else {  \
+        std::cerr << "Failed zenoh query for key expression: "<< expr << std::endl; \
+    } \
+    
+// end of ZENOH
 
 class FmuInstance {
 public:
@@ -30,24 +88,6 @@ const char* fmi3GetVersion() {
 }
 
 
-void queryZenoh(zenoh::net::Session& session, const std::string& query) {
-    try {
-        auto syncGet = session.get(query);
-        auto responses = syncGet.wait();
-        // Iterate through and process each response
-        for (auto &response : responses) {
-            if (response.kind() == zenoh::net::GET) {
-                std::cout << "Received response: " << response.payload().value().to_string() << std::endl;
-            } else {
-                std::cerr << "Received non-get response" << std::endl;
-            }
-        }
-    } catch (const std::exception& e) {
-        std::cerr << "Exception during zenoh get: " << e.what() << std::endl;
-    }
-}
-
-
 fmi3Instance fmi3InstantiateCoSimulation(
     fmi3String                     instanceName,
     fmi3String                     instantiationToken,
@@ -62,20 +102,12 @@ fmi3Instance fmi3InstantiateCoSimulation(
     fmi3LogMessageCallback         logMessage,
     fmi3IntermediateUpdateCallback intermediateUpdate) {
     
-
-    Config config;
-    session = except<Session>(open(std::move(config)));
-    session.get("rpc/demo/query")
-
-    proto::Empty request;
-    proto::Instance reply;
-    grpc::ClientContext context;
-    grpc::Status status = client->instantiateCoSimulation(&context, request, &reply);
-    if (!status.ok()) {
-        std::cerr << status.error_message() << std::endl;
-        return nullptr;
-    }
-    FmuInstance* fmu_instance = new FmuInstance(reply.key());
+    
+    startZenoh();
+    proto::Empty input;
+    proto::Instance output;
+    ZENOH_FMI3_QUERY("fmi3InstantiateCoSimulation")
+    FmuInstance* fmu_instance = new FmuInstance(output.key());
     return reinterpret_cast<fmi3Instance>(fmu_instance);
 }
 
--- src/liaison_client.cpp
+++ src/liaison_client.cpp
@@ -9,56 +9,38 @@
 
 
 std::unique_ptr<zenoh::Session> z_client;
+std::string responder_id = "demo";
 
 
-std::vector<zenohc::Sample> extractSamples(zenoh::ClosureReplyChannelRecv& recv) {
-    std::vector<zenohc::Sample> samples; // Vector to store received samples
-    zenoh::Reply reply(nullptr); // Initialize a zenoh::Reply object
-
-    while (recv(reply)) { // Use while loop to process received replies
-        if (reply.check()) { // Check if the reply is valid
-            auto sample = zenohc::expect<zenoh::Sample>(reply.get()); // Extract sample from reply
-            samples.push_back(sample); // Add the valid sample to the vector
-        } else {
-            break; // Exit loop if no more valid replies are expected
-        }
-    }
-
-    return samples; // Return the vector of samples
-}
-
-
-// Function with 'value'
-std::vector<zenohc::Sample> queryZenoh(
-    const std::string& keystr,
-    const char* params,  
-    const zenohc::Value& value,
-    int n_replies = 10
-) {
-    auto [send, recv] = zenohc::reply_fifo_new(n_replies);
-    zenoh::GetOptions opts;
-    opts.set_value(value); // Set the value into options
-
-    z_client->get(keystr, params, std::move(send), opts); 
-
-    return extractSamples(recv);
-}
-
-std::vector<zenohc::Sample> queryZenoh(
-    const std::string& keystr,
-    const char* params = "",  
-    int n_replies = 10
-) {
-    auto [send, recv] = zenohc::reply_fifo_new(n_replies);
-    zenoh::GetOptions opts; // Default options are used
-
-    z_client->get(keystr, params, std::move(send), opts); 
-
-    return extractSamples(recv);
-}
+// This macro assumes that:
+//  1.  the 'input' and 'output' of protobuf messages
+// are declared
+#define ZENOH_FMI3_QUERY(FMI3_FUNCTION) \
+    size_t input_size = input.ByteSizeLong(); \
+    std::vector<uint8_t> buffer(input_size); \
+    input.SerializeToArray(buffer.data(), input_size); \
+    std::string expr = "rpc/" + responder_id + "/" + FMI3_FUNCTION; \
+    auto [send, recv] = zenohc::reply_fifo_new(1);   \
+    const char* params = ""; \
+    zenoh::GetOptions options; \
+    zenoh::Value value(buffer); \
+    options.set_value(value); \
+    z_client->get(          \
+        expr,    \
+        params,             \
+        std::move(send),            \
+        options);                        \
+    zenoh::Reply reply(nullptr);  \
+    recv(reply);  \
+    if (reply.is_ok()) { \
+        zenohc::Sample sample = zenohc::expect<zenoh::Sample>(reply.get()); \
+        output.ParseFromArray(sample.payload.start, sample.payload.len); \
+    } else {  \
+        std::cerr << "Failed zenoh query for keyexpr: "<< expr << std::endl; \
+    } \
+    
 
 
-// Helper function to extract samples from replies
 
 
 void startZenoh(){
@@ -74,30 +56,16 @@ void startZenoh(){
     }
 }
 
-
-void printSamples(std::vector<zenoh::Sample> samples) {
-    
-   
-    std::cout << " samples size " << samples.size()  << std::endl;
-    for (const auto& sample : samples) {
-        std::cout << "start "  << std::endl;
-        proto::Instance instance;
-        instance.ParseFromArray(sample.payload.start, sample.payload.len);
-        std::cout << "Instance number: " << instance.key() << std::endl; 
-    }
-}
-
 int main(int argc, char **argv) {
 
     startZenoh();
-    auto samples = queryZenoh("rpc/demo/query");
-    printSamples(samples);
-    std::string payload = "Hello Zenoh!";
-    zenohc::Value value(payload); 
-    // Key string for the Zenoh query
-    std::string key = "rpc/demo/query";
-    auto samples2 = queryZenoh(key,"foobar",value);
-    printSamples(samples2);
+    
+    proto::Empty input;
+    input.set_value(333);
+    proto::Instance output;
+    ZENOH_FMI3_QUERY("fmi3InstantiateCoSimulation")
+
+    std::cout << "Instance number: " << output.key() << std::endl; 
    
     z_client.reset();
 
